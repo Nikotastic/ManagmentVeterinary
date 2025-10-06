743b89b05b8381320e72cd7b15814aaac943a416
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-10-06 17:56:12 -0500

    Revert "User history 2: Implement Linq exercises"
    This reverts commit 12e6afdd3fa5ce8a81fdb26109336cbe7f4d6a95.

--- ManagmentVeterinary/Services/PetService.cs
+++ ManagmentVeterinary/Services/PetService.cs
@@ -38,14 +38,8 @@ public class PetService
         Console.WriteLine("\n--- Delete Pet ---");
         Console.Write("Enter patient ID: ");
         string patientIdInput = Console.ReadLine()!;
-        if (!int.TryParse(patientIdInput, out int patientId))
-        {
-            Console.WriteLine("Invalid patient ID. Please enter a number.");
-            return;
-        }
-
+        int patientId = int.Parse(patientIdInput);
         // Validacion del paciente
-        
 
         if (!patientDictionary.ContainsKey(patientId))
         {
