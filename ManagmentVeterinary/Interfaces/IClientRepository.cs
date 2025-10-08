67b6412e3fc0e990f1ccf8c1ba5ddb7c91daa774
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-10-08 17:34:14 -0500

    I modified the file so that it can be the connection to the repository

--- ManagmentVeterinary/Interfaces/IClientRepository.cs
+++ ManagmentVeterinary/Interfaces/IClientRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ManagmentVeterinary.Interfaces;
+﻿using ManagmentVeterinary.Models;
+using ManagmentVeterinary.Services;
+namespace ManagmentVeterinary.Interfaces;
 
 public interface IClientRepository
 {
-    
+    void AddClient(Client client);
+    List<Client> GetAll();
+    Client? GetById(int id);
+    void Update(Client client);
+    void Delete(int id);
+    IEnumerable<Client> FindByName(string name);
 }
