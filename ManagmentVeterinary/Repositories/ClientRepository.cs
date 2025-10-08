8dc40e966d7812a8825943e2c2b497d73235581c
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-10-08 00:02:11 -0500

    Refactor: Keep ClientRepository focused only on CRUD operations following the Repository pattern

--- ManagmentVeterinary/Repositories/ClientRepository.cs
+++ ManagmentVeterinary/Repositories/ClientRepository.cs
@@ -3,12 +3,41 @@ using ManagmentVeterinary.Models;
 
 namespace ManagmentVeterinary.Interfaces.Repositories;
 
-public interface IClientRepository : IRepository<Client>
+public class ClientRepository : IClientRepository
 {
-    void RegisterClient(Client client);
-    IEnumerable<Client> FindByName(string name);
-    Client? GetClientById(int id);
-    void UpdateClient(Client client);
-    void DeleteClient(int id);
-    
+    public void AddClient(Client client)
+    {
+        Data.Database.Clients.Add(client.IdClient, client);
+    }
+
+    public List<Client> GetAll()
+    {
+        return Data.Database.Clients.Values.ToList();
+    }
+
+    public Client? GetById(int id)
+    {
+        Data.Database.Clients.TryGetValue(id, out var client);
+        return client;
+    }
+
+    public void Update(Client client)
+    {
+        if (Data.Database.Clients.ContainsKey(client.IdClient))
+        {
+            Data.Database.Clients[client.IdClient] = client;
+        }
+    }
+
+    public void Delete(int id)
+    {
+        Data.Database.Clients.Remove(id);
+    }
+
+    public IEnumerable<Client> FindByName(string name)
+    {
+        return Data.Database.Clients.Values
+            .Where(c => c.Name.Contains(name, StringComparison.OrdinalIgnoreCase));
+    }
 }
+
