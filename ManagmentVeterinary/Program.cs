c3a04fceebba819ef5e45762e392e6ff6807c5a6
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-10-08 17:41:32 -0500

    leaving the code cleaner

--- ManagmentVeterinary/Program.cs
+++ ManagmentVeterinary/Program.cs
@@ -1,36 +1,4 @@
-﻿using ManagmentVeterinary.Services;
+using ManagmentVeterinary.Menu;
 
-// Inicialización de servicios
-PatientService patientService = new PatientService();
-PetService petService = new PetService();
-
-string option;
-
-do
-{
-    Console.WriteLine("\n--- What would you like to do? ---");
-    Console.WriteLine(@"1. Register patient
-2. List patients
-3. Search patient
-4. Add pet to patient
-5. Delete pet to patient
-6. Update pet to patient
-7. Exit");
-    option = Console.ReadLine()!;
-
-    switch (option)
-    {
-        case "1": patientService.RegisterPatient(); break;
-        case "2": patientService.ListPatients(); break;   
-        case "3": patientService.SearchPatientByName(); break;
-        case "4": petService.AddPet(); break;
-        case "5": petService.RemovePet(); break; 
-        case "6": petService.UpdatePet(); break; 
-        case "7":
-            Console.WriteLine("Goodbye");
-            break;
-        default:
-            Console.WriteLine("Invalid option");
-            break;
-    }
-} while (option != "7");
+MenuMain menuMain = new MenuMain();
+menuMain.ShowMenu();
--- ManagmentVeterinary/Utils/MenuMain.cs
+++ ManagmentVeterinary/Utils/MenuMain.cs
@@ -2,5 +2,40 @@
 
 public class MenuMain
 {
-    
+    public void ShowMenu()
+    {
+        int opcion;
+        do
+        {
+            Console.WriteLine("\n--- MENU PRINCIPAL ---");
+            Console.WriteLine("1. Clientes");
+            Console.WriteLine("2. Mascotas");
+            Console.WriteLine("3. Salir");
+            Console.Write("\nSeleccione una opción: ");
+
+            if (!int.TryParse(Console.ReadLine(), out opcion))
+            {
+                Console.WriteLine("Invalid option, please try again.");
+                continue;
+            }
+
+            switch (opcion)
+            {
+                case 1:
+                    MenuClient menuClient = new MenuClient();
+                    menuClient.ShowMenu();
+                    break;
+                case 2:
+                    MenuPet menuPet = new MenuPet();
+                    menuPet.MostrarMenu();
+                    break;
+                case 3:
+                    Console.WriteLine("Saliendo del menú principal...");
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida.");
+                    break;
+            }
+        } while (opcion != 3);
+    }
 }
