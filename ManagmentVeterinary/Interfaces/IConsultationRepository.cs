6c39bfa11e7dd03ed689511e11d5dfa233d8d3d5
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-10-10 01:20:51 -0500

    I implemented the interface of the consultation to conect the database

---
+++ ManagmentVeterinary/Interfaces/IConsultationRepository.cs
@@ -0,0 +1,12 @@
+﻿using ManagmentVeterinary.Models;
+
+namespace ManagmentVeterinary.Interfaces;
+
+public interface IConsultationRepository
+{
+    void AddConsultation (Consultation consultation);
+    void Delete(int id);
+    void Update(Consultation consultation);
+    Consultation GetById(int id);
+    List<Consultation> GetAll();
+}
